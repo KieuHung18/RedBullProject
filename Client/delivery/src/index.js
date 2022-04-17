ed7cf7b8c482235bee43215fd0a648a580b5a492
Hoangabcxyz <nguyenhoang1600@gmail.com>
2022-04-17 20:33:44 +0700

    Hoang-Login
    hoang ne2

--- Client/delivery/src/index.js
+++ Client/delivery/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { PackageList } from './page/PackageList/PackageList';
-
+// import { PackageList } from './page/PackageList/PackageList';
+import { Login } from './page/Login/Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PackageList />
+    <Login />
   </React.StrictMode>
 );
 
---
+++ Client/delivery/src/page/Login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export class Login extends React.Component{
+    render(){
+     return(
+        <h1> hoang ne </h1>
+     );    
+    }
+}
