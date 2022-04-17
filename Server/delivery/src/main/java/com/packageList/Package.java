79d30b7740d1d8c35a4348201e6be3efb42bde29
Nguyễn Văn Quyết <ngvanquyet2000@gmail.com>
2022-04-17 14:54:53 +0700

    first commit
    hello

---
+++ Server/delivery/src/main/java/com/packageList/Package.java
@@ -0,0 +1,17 @@
+package com.packageList;
+
+public class Package {
+	private String address;
+
+	public Package(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
