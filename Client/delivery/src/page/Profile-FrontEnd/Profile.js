d7b3ec31e1603c96843aa5b8a8dc58e5fb7f01d8
Phat <18130172@st.hcmuaf.edu.vn>
2022-04-17 19:59:43 +0700

    Phat
    Profile

---
+++ Client/delivery/src/page/Profile-FrontEnd/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {}from 'react-bootstrap';//{Card,ListGroupItem,ListGroup} 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Profile.css';
+export class Profile extends React.Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    <label>Hello </label>
+                </div>
+            </div>
+        );
+    }
+}
