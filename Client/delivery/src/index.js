9b068cd1d9c43701d3aebf7009ef24e848398eeb
kieuhung <kieuhungcm2015@gmail.com>
2022-04-19 22:22:18 +0700

    working change
    commit

--- Client/delivery/src/index.js
+++ Client/delivery/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { PackageList } from './page/PackageList/PackageList';
+// import PackageList from './page/PackageList/PackageList';
+import {PackageList} from './page/PackageList/PackageList';
+import  {PackageDetail}  from './page/PackageDetail/PackageDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
