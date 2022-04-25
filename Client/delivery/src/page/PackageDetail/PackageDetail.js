f258380602438fc2fed8c77dff2a3797cc651b7f
buithienhao-nlu <18130069@st.hcmuaf.edu.vn>
2022-04-25 23:10:09 +0700

    Commit3.2
    update

--- Client/delivery/src/page/PackageDetail/PackageDetail.js
+++ Client/delivery/src/page/PackageDetail/PackageDetail.js
@@ -1,19 +1,14 @@
 import React from "react";
-import {
-  Card,
-  ListGroup,
-  ListGroupItem,
-  Tab,
-  Container,
-} from "react-bootstrap";
-import Tabs from "react-bootstrap/Tabs";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./PackageDetail.css";
-import jquery from "jquery";
-
+import $ from "jquery";
 var packageinfo;
 packageinfo = {
-  address: "Viet Nam/Binh Duong/Di an",
+  pro: "Ho Chi Minh city",
+  district: "Quan 1",
+  ward: "Dakao",
+  street: "Cong vien Le Van Tam",
   deliveryDate: "Not Delivery",
   receiveDate: "16/4/2022",
   price: 200000,
@@ -21,6 +16,8 @@ packageinfo = {
 };
 var customer;
 customer = { phone: "0993826268", name: "cusName" };
+
+$(function () {});
 
 export class PackageDetail extends React.Component {
   constructor(props) {
@@ -53,56 +50,74 @@ export class PackageDetail extends React.Component {
   // }
   render() {
     return (
-      <Container id="#container-detail">
-        <Card id="card-detail">
-          <Tabs defaultActiveKey="infomation">
-            <Tab eventKey="infomation" title="Infomation">
-              <Card.Body className="package-detail-body">
-                <h1 class="text-h2">Package Infomation</h1>
-                <ListGroup className="list-group-flush">
-                  <Card.Title>Address</Card.Title>
-                  <ListGroupItem>
-                    <p class="item">{packageinfo.address}</p>
-                  </ListGroupItem>
+      <Container id="container-detail">
+        <Row class="">
+          <Col md={6}>
+            <table>
+              <thead>
+                <tr>
+                  <th>
+                    <h1>Package Info</h1>
+                  </th>
+                  <th></th>
+                </tr>
+              </thead>
 
-                  <Card.Title>Delivery Date</Card.Title>
-                  <ListGroupItem>
-                    <p class="item">{packageinfo.deliveryDate}</p>
-                  </ListGroupItem>
+              <tr>
+                <td>Province/City</td>
+                <td>{packageinfo.pro}</td>
+              </tr>
+              <tr>
+                <td>District</td>
+                <td>{packageinfo.district}</td>
+              </tr>
+              <tr>
+                <td>Wards</td>
+                <td>{packageinfo.ward}</td>
+              </tr>
+              <tr>
+                <td>Street/House</td>
+                <td>{packageinfo.street}</td>
+              </tr>
+              <tr>
+                <td>Delivery</td>
+                <td>{packageinfo.deliveryDate}</td>
+              </tr>
+              <tr>
+                <td>Receive Date</td>
+                <td>{packageinfo.receiveDate}</td>
+              </tr>
+              <tr>
+                <td>Price</td>
+                <td>{packageinfo.price}</td>
+              </tr>
+              <tr>
+                <td>Status</td>
+                <td>{packageinfo.status}</td>
+              </tr>
+              <tr>
+                <td>Phone</td>
+                <td>{customer.phone}</td>
+              </tr>
+              <tr>
+                <td>Name</td>
+                <td>{customer.name}</td>
+              </tr>
+            </table>
+            <div id="group-btn">
+              <Button variant="success">
+                successful delivery confirmation
+              </Button>{" "}
+              <Button variant="warning">delivery failure</Button>{" "}
+            </div>
+          </Col>
 
-                  <Card.Title>Receive Date</Card.Title>
-                  <ListGroupItem>
-                    <p className="item">{packageinfo.receiveDate}</p>
-                  </ListGroupItem>
-                  <Card.Title>Price</Card.Title>
-                  <ListGroupItem>
-                    <p class="item"> {packageinfo.price}</p>
-                  </ListGroupItem>
-
-                  <Card.Title>Status</Card.Title>
-                  {packageinfo.status ? (
-                    <ListGroupItem className="recive-date">
-                      <p class="item ">Delivered</p>
-                    </ListGroupItem>
-                  ) : (
-                    <ListGroupItem className="recive-date">
-                      <p class="item ">Pedding...</p>
-                    </ListGroupItem>
-                  )}
-                  <Card.Title>Contact</Card.Title>
-                  <ListGroupItem>
-                    <div class="item">Customer: {customer.name}</div>
-                    <div class="item">Phone number: {customer.phone}</div>
-                  </ListGroupItem>
-                  <ListGroupItem></ListGroupItem>
-                </ListGroup>
-              </Card.Body>
-            </Tab>
-            <Tab eventKey="map" title="Map">
-              <h1>MAP</h1>
-            </Tab>
-          </Tabs>
-        </Card>
+          {/* This is map  */}
+          <Col md={6} id="map">
+            This is map
+            <img src="http://laptrinhphp.info/uploads//images/2019/google-maps-banner.jpg"></img>
+          </Col>
+        </Row>
       </Container>
     );
   }
