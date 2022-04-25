8fb4af8f6f2359b1197f0e77d500a67c85ce90e9
buithienhao-nlu <18130069@st.hcmuaf.edu.vn>
2022-04-25 17:05:54 +0700

    Commit2

--- Client/delivery/src/page/PackageDetail/PackageDetail.js
+++ Client/delivery/src/page/PackageDetail/PackageDetail.js
@@ -1,86 +1,109 @@
 import React from "react";
-import {Card,ListGroup,ListGroupItem,Tab} from 'react-bootstrap';
-import Tabs from 'react-bootstrap/Tabs'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './PackageDetail.css'
+import {
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Tab,
+  Container,
+} from "react-bootstrap";
+import Tabs from "react-bootstrap/Tabs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./PackageDetail.css";
 import jquery from "jquery";
 
 var packageinfo;
-packageinfo={address:"Viet Nam/Binh Duong/Di an",deliveryDate: "Not Delivery",receiveDate:"16/4/2022",price: 200000,status: false}
+packageinfo = {
+  address: "Viet Nam/Binh Duong/Di an",
+  deliveryDate: "Not Delivery",
+  receiveDate: "16/4/2022",
+  price: 200000,
+  status: false,
+};
 var customer;
-customer={phone:"0993826268",name:"cusName"};
+customer = { phone: "0993826268", name: "cusName" };
 
-export class PackageDetail extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = { loadData: true };
-      }
-    // componentDidMount() {
-    // var display=this;
-    // jquery.ajax({
-    //     type: "GET",
-    //     url: "http://localhost:8080/delivery/package",
-    //     data: {packageID:"p1"},
-    //     success: function(res){
-    //     if(res.result=="SUCCESS"){
-    //         if(display.state.loadData){
-    //             packageinfo={address: res.response.address,deliveryDate: res.response.deliveryDate,receiveDate: res.response.receiveDate,price:  res.response.price,status:  res.response.status}
-    //             customer={phone: res.response.customerName,name: res.response.customerPhone};
-    //         display.setState({ loadData: false });
-    //     }
-            
-    //     }
-    //     else{console.log("fail");
-    //         //redirect
-    //     }
-    //     },
-    //     error: function(){
-    //         console.log("error");
-    //     }
-    // });
-    // }
-    render(){
-         return(
-            <div className="package-detail-container">
-            <Card  border="dark">
-                <Tabs defaultActiveKey="infomation">
-                    <Tab eventKey="infomation" title="Infomation">
-                        <Card.Body className="package-detail-body">
-                        <h1>Package Infomation</h1>
-                            <ListGroup className="list-group-flush">
-                                <Card.Title>Address</Card.Title>
-                                <ListGroupItem>{packageinfo.address}</ListGroupItem>
+export class PackageDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loadData: true };
+  }
+  // componentDidMount() {
+  // var display=this;
+  // jquery.ajax({
+  //     type: "GET",
+  //     url: "http://localhost:8080/delivery/package",
+  //     data: {packageID:"p1"},
+  //     success: function(res){
+  //     if(res.result=="SUCCESS"){
+  //         if(display.state.loadData){
+  //             packageinfo={address: res.response.address,deliveryDate: res.response.deliveryDate,receiveDate: res.response.receiveDate,price:  res.response.price,status:  res.response.status}
+  //             customer={phone: res.response.customerName,name: res.response.customerPhone};
+  //         display.setState({ loadData: false });
+  //     }
 
-                                <Card.Title>Delivery Date</Card.Title>
-                                <ListGroupItem>{packageinfo.deliveryDate}</ListGroupItem>
+  //     }
+  //     else{console.log("fail");
+  //         //redirect
+  //     }
+  //     },
+  //     error: function(){
+  //         console.log("error");
+  //     }
+  // });
+  // }
+  render() {
+    return (
+      <Container id="#container-detail">
+        <Card id="card-detail">
+          <Tabs defaultActiveKey="infomation">
+            <Tab eventKey="infomation" title="Infomation">
+              <Card.Body className="package-detail-body">
+                <h1 class="text-h2">Package Infomation</h1>
+                <ListGroup className="list-group-flush">
+                  <Card.Title>Address</Card.Title>
+                  <ListGroupItem>
+                    <p class="item">{packageinfo.address}</p>
+                  </ListGroupItem>
 
-                                <Card.Title>Receive Date</Card.Title>
-                                <ListGroupItem>{packageinfo.receiveDate}</ListGroupItem>
-                                <Card.Title>Price</Card.Title>
-                                <ListGroupItem>{packageinfo.price}</ListGroupItem>
+                  <Card.Title>Delivery Date</Card.Title>
+                  <ListGroupItem>
+                    <p class="item">{packageinfo.deliveryDate}</p>
+                  </ListGroupItem>
 
-                                <Card.Title>Status</Card.Title>
-                                {packageinfo.status?
-                                <ListGroupItem className="recive-date">Delivered</ListGroupItem>:
-                                <ListGroupItem className="recive-date">Pedding...</ListGroupItem>
-                                }
-                                <Card.Title>Contact</Card.Title>
-                                <ListGroupItem >
-                                    <div>Customer: {customer.name}</div>
-                                    <div>Phone number: {customer.phone}</div>
-                                </ListGroupItem>
-                                <ListGroupItem></ListGroupItem>
-                            </ListGroup>
-                        </Card.Body>
-                    </Tab>
-                    <Tab eventKey="map" title="Map">
-                        <h1>MAP</h1>
-                    </Tab>
-                </Tabs>
+                  <Card.Title>Receive Date</Card.Title>
+                  <ListGroupItem>
+                    <p className="item">{packageinfo.receiveDate}</p>
+                  </ListGroupItem>
+                  <Card.Title>Price</Card.Title>
+                  <ListGroupItem>
+                    <p class="item"> {packageinfo.price}</p>
+                  </ListGroupItem>
 
-            
-            </Card>
-          </div>
-         );
-     }
- }
+                  <Card.Title>Status</Card.Title>
+                  {packageinfo.status ? (
+                    <ListGroupItem className="recive-date">
+                      <p class="item ">Delivered</p>
+                    </ListGroupItem>
+                  ) : (
+                    <ListGroupItem className="recive-date">
+                      <p class="item ">Pedding...</p>
+                    </ListGroupItem>
+                  )}
+                  <Card.Title>Contact</Card.Title>
+                  <ListGroupItem>
+                    <div class="item">Customer: {customer.name}</div>
+                    <div class="item">Phone number: {customer.phone}</div>
+                  </ListGroupItem>
+                  <ListGroupItem></ListGroupItem>
+                </ListGroup>
+              </Card.Body>
+            </Tab>
+            <Tab eventKey="map" title="Map">
+              <h1>MAP</h1>
+            </Tab>
+          </Tabs>
+        </Card>
+      </Container>
+    );
+  }
+}
