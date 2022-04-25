84e1e9d3a7f8896b4f032466554bc3c66cce05e7
buithienhao-nlu <18130069@st.hcmuaf.edu.vn>
2022-04-25 17:15:21 +0700

    Revert "Commit2"
    This reverts commit 8fb4af8f6f2359b1197f0e77d500a67c85ce90e9.

--- Client/delivery/src/index.js
+++ Client/delivery/src/index.js
@@ -1,44 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import "../node_modules/font-awesome/css/font-awesome.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
-import { PackageList } from "./page/PackageList/PackageList";
-import Footer from "./page/Footer/Footer";
-import Navbar from "./page/Navbar/Navbar";
-import AboutUS from "./page/AboutUsPage/AboutUs.jsx";
-import { Profile } from "./page/Profile-FrontEnd/Profile";
-import { PackageDetail } from "./page/PackageDetail/PackageDetail";
-import NotFound from "./page/NotFound/NotFound";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "./page/HomePage/HomePage";
-import Login from "./page/Login/Login";
-const routing = (
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="home" element={<HomePage />} />
-      <Route path="login" element={<Login />} />
-      <Route path="packagelist" element={<PackageList />} />
-      <Route path="package/:id" element={<PackageDetail />} />
-      <Route path="aboutus" element={<AboutUS />} />
-      <Route path="profile" element={<Profile />} />
-    </Routes>
-  </BrowserRouter>
-);
-const page = (
-  <div className="ract-root">
-    <Navbar />
+import { PackageList } from './page/PackageList/PackageList';
+import Footer from './page/Footer/Footer';
+import Navbar from './page/Navbar/Navbar';
+import AboutUS from './page/AboutUsPage/AboutUs.jsx';
+import { Profile } from './page/Profile-FrontEnd/Profile';
+import  {PackageDetail}  from './page/PackageDetail/PackageDetail';
+import NotFound from './page/NotFound/NotFound';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {HomePage} from './page/HomePage/HomePage';
+import Login from './page/Login/Login';
+const routing=(
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="home" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="packagelist" element={<PackageList />} />
+        <Route path="package/:id" element={<PackageDetail />} />
+        <Route path="aboutus" element={<AboutUS />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+const page=(
+    <div className='ract-root'>
+    <Navbar/>
     {routing}
-    <Footer />
-  </div>
+    <Footer/>
+    </div>
+  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {page}
+  </React.StrictMode>
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>{page}</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- Client/delivery/src/page/HomePage/HomePage.js
+++ Client/delivery/src/page/HomePage/HomePage.js
@@ -6,49 +6,26 @@ export class HomePage extends React.Component {
   render() {
     return (
       <Container id="container">
-        {/* <Row class="row">
+        <Row class="row">
           <h3 class="text-bold">Header</h3>
         </Row>
         <hr></hr>
         <Row class="row">
           <h3 class="text-bold">Navbars</h3>
         </Row>
-        <hr></hr> */}
-
-        {/* HIGHLIGHT  */}
+        <hr></hr>
         <Row class="row">
-          <h2 class="text-h2">Highlights</h2>
+          <h3 class="text-bold">Hightlights</h3>
         </Row>
         <Row>
-          <Col sm={12} md={4} lg={4}>
-            <div>
-              <i class="fa-solid fa-award icon"></i>
-            </div>
-            <h5 class="text-h5">The best for delivery</h5>
-            {/* <p>
-              Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus
-              consectetuer turpis, suspendisse.
-            </p> */}
+          <Col>
+            <h5>The best for delivery</h5>
           </Col>
-          <Col sm={12} md={4} lg={4}>
-            <div>
-              <i class="fa-solid fa-truck-fast icon"></i>
-            </div>
-            <h5 class="text-h5">Fast delivery, high success rate</h5>
-            {/* <p>
-              Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus
-              consectetuer turpis, suspendisse.
-            </p> */}
+          <Col>
+            <h5>Fast delivery, high success rate</h5>
           </Col>
-          <Col sm={12} md={4} lg={4}>
-            <div>
-              <i class="fa-solid fa-signal icon"></i>
-            </div>
-            <h5 class="text-h5">24/7 online management system</h5>
-            {/* <p>
-              Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus
-              consectetuer turpis, suspendisse.
-            </p> */}
+          <Col>
+            <h5>24/7 online management system</h5>
           </Col>
         </Row>
         <hr></hr>
@@ -59,66 +36,12 @@ export class HomePage extends React.Component {
         </Row>
         <hr></hr>
         <Row class="row">
-          <h3 class="text-h2">Customer testimonials</h3>
+          <h3 class="text-bold">Customer testimonials</h3>
         </Row>
         <Row>
-          <Card id="card" class="card">
-            <Row>
-              <Col sm={12} md={2}>
-                <Card.Title>
-                  <i class="fa-solid fa-message icon2"></i>
-                </Card.Title>
-              </Col>
-              <Col sm={12} md={10}>
-                {" "}
-                <Card.Body>
-                  <blockquote className="blockquote mb-0 text-p">
-                    <p>
-                      {" "}
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Integer posuere erat a ante.{" "}
-                    </p>
-                    <footer className="blockquote-footer">
-                      Someone famous in{" "}
-                      <cite title="Source Title">Source Title</cite>
-                    </footer>
-                  </blockquote>
-                </Card.Body>
-              </Col>
-            </Row>
-          </Card>
-        </Row>
-        <Row>
-          <Card id="card">
-            <Row>
-              <Col sm={12} md={2}>
-                <Card.Title>
-                  <i class="fa-solid fa-message icon2"></i>
-                </Card.Title>
-              </Col>
-              <Col sm={12} md={10}>
-                {" "}
-                <Card.Body>
-                  <blockquote className="blockquote mb-0 text-p">
-                    <p>
-                      {" "}
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Integer posuere erat a ante.{" "}
-                    </p>
-                    <footer className="blockquote-footer">
-                      Someone famous in{" "}
-                      <cite title="Source Title">Source Title</cite>
-                    </footer>
-                  </blockquote>
-                </Card.Body>
-              </Col>
-            </Row>
-          </Card>
-        </Row>
-        {/* <Row>
           <Card id="card">
             <Card.Body>
-              <blockquote className="blockquote mb-0 text-h5">
+              <blockquote className="blockquote mb-0">
                 <p>
                   {" "}
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -131,10 +54,27 @@ export class HomePage extends React.Component {
               </blockquote>
             </Card.Body>
           </Card>
-        </Row> */}
+        </Row>
+        <Row>
+          <Card id="card">
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer posuere erat a ante.{" "}
+                </p>
+                <footer className="blockquote-footer">
+                  Someone famous in{" "}
+                  <cite title="Source Title">Source Title</cite>
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        </Row>
         <hr></hr>
         <Row class="row">
-          <h3 class="text-h2">Contact Us</h3>
+          <h3 class="text-bold">Get in touch</h3>
         </Row>
         <Row class="row">
           <Form id="form">
@@ -162,15 +102,15 @@ export class HomePage extends React.Component {
               ></Form.Control>
             </Form.Group>
 
-            <Button class="btn-submit" id="btn-submit" type="submit">
-              Send Message
+            <Button id="btn-submit" variant="primary" type="submit">
+              Submit
             </Button>
           </Form>
         </Row>
-        {/* <hr></hr>
+        <hr></hr>
         <Row class="row">
           <h3 class="text-bold">Footer</h3>
-        </Row> */}
+        </Row>
       </Container>
     );
   }
