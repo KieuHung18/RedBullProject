3c7e566708922b62f6500aceb3092d1a2dbdc462
buithienhao-nlu <18130069@st.hcmuaf.edu.vn>
2022-04-28 08:53:47 +0700

    Commit temp
    Waiting for background-image

--- Client/delivery/src/page/Login/Login.js
+++ Client/delivery/src/page/Login/Login.js
@@ -1,87 +1,71 @@
-import Container from 'react-bootstrap/Container';
-import {Form,Button} from 'react-bootstrap';
-import './Login.css';
-import React,{useState} from 'react';
-import  logo from '../../assets/LogoCompany.png'
-import accountimg from '../../assets/images/account.svg'
-import passimg from '../../assets/images/password.svg'
-import jquery from 'jquery';
-import {useNavigate } from 'react-router-dom'; 
+import Container from "react-bootstrap/Container";
+import { Form, Button, Row } from "react-bootstrap";
+import "./Login.css";
+import React, { useState } from "react";
+import logo from "../../assets/LogoCompany.png";
+import accountimg from "../../assets/images/account.svg";
+import passimg from "../../assets/images/password.svg";
+import jquery from "jquery";
+import { useNavigate } from "react-router-dom";
 // asdasdasd
 function Login() {
   // const [account,setAccount]=useState("")
   // const [password,setPassword]=useState("")
-  return (
-    <Component navigate={useNavigate()}/>
-  );
+  return <Component navigate={useNavigate()} />;
 }
-class Component extends React.Component{
-  constructor(props){
+class Component extends React.Component {
+  constructor(props) {
     super(props);
-    this.doLogin=this.doLogin.bind(this);
+    this.doLogin = this.doLogin.bind(this);
   }
   doLogin(event) {
     event.preventDefault();
-    var display=this;
+    var display = this;
     jquery.ajax({
-        type: "POST",
-        url: "http://localhost:8080/delivery/authenticateUser",
-        data: {account: jquery("#account").val(), password: jquery("#password").val()},
-        xhrFields: {
-          withCredentials: true
-          },
-          crossDomain: true,
-        success: function(response){
-          if(response.result!="FAIL"){
-            localStorage.setItem("user",response.response);
-            display.props.navigate("/profile");
-            window.location.reload(false);
-          }
-          else{
-            alert("Wrongg user name or password")
-          }
-        },
-        error: function(){
-          console.log("error");
+      type: "POST",
+      url: "http://localhost:8080/delivery/authenticateUser",
+      data: {
+        account: jquery("#account").val(),
+        password: jquery("#password").val(),
+      },
+      xhrFields: {
+        withCredentials: true,
+      },
+      crossDomain: true,
+      success: function (response) {
+        if (response.result != "FAIL") {
+          localStorage.setItem("user", response.response);
+          display.props.navigate("/profile");
+          window.location.reload(false);
+        } else {
+          alert("Wrongg user name or password");
         }
+      },
+      error: function () {
+        console.log("error");
+      },
     });
-    // 
+    //
   }
-  render(){
-    return(
-      <Container id="main-container" className="main-container">
-      <div>
-        <div className="imgs">
-          <div className="container-image">
-            <img src={logo} alt="logo" className="logo"/>
-      </div>
-    </div>
-    <h1 className="header">Login Page</h1>
-    <Form onSubmit={this.doLogin}>
-      <Form.Group  id="sign-in-account"></Form.Group>
-      <div class="txt-field1">
-        <img src={accountimg} alt="accountimg" className="accountandpass"/>
-        <input  type="text" id="account" className="login-input account" placeholder="Enter your account"></input>
-        <span></span>
+  render() {
+    return (
+      <Row class="row">
+        <Form id="form">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="User name" />
+          </Form.Group>
 
-        </div>
-      <Form.Group  id="sign-in-password"></Form.Group>
-      <div class="txt-field2">
-      <div className="distance">
-        <img src={passimg} alt="passimg" className="accountandpass"/>  
-          <input id="password" type="password" className="login-input pass" placeholder="Enter your password"></input>
-          <span></span>
-        </div>
-        </div>
-        
-      <div>
-      <div >
-        <button className='login-button' variant="primary" type="submit">Login</button>
-       </div>
-      </div>
-      </Form>
-    </div>
-    </Container>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check id="checkbox" type="checkbox" label="Remember" />
+          </Form.Group> */}
+          <Button class="btn-submit" id="btn-submit" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Row>
     );
   }
 }
